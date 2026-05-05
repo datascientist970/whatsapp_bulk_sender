6bf093df208e44cd7fd71d557bcc198391ab41be
Muhammad Dawood <mixlibofficial@gmail.com>
2026-05-05 12:48:35 +0500

    Update urls.py

--- whatsapp_sender/urls.py
+++ whatsapp_sender/urls.py
@@ -1,13 +1,13 @@
-# core/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('api/debug/', views.get_debug_info, name='debug'),
-    path('api/check-connection/', views.check_connection, name='check_connection'),
-    path('api/reconnect/', views.reconnect_whatsapp, name='reconnect'),
-    path('api/generate-message/', views.generate_ai_message, name='generate_message'),
-    path('api/send-messages/', views.send_messages, name='send_messages'),
-    path('api/check-numbers/', views.check_numbers, name='check_numbers'),
+    path('', views.index),
+    path('api/check-connection/', views.check_connection),
+    path('api/generate-message/', views.generate_message),
+    path('api/send-messages/', views.send_messages),
+    path('api/check-numbers/', views.check_numbers),
+    path('api/debug/', views.get_debug),
+    path('api/task-status/', views.get_task_status),
+    path('api/cancel-task/', views.cancel_task),
 ]
